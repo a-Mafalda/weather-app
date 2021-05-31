62706887764c9c7c29344286547340df67ebcbdd
Mafalda <84099790+a-Mafalda@users.noreply.github.com>
2021-05-31 11:15:40 +0100

    added forecast

--- script.js
+++ script.js
@@ -22,23 +22,37 @@ let todayDate = date.getDate();
 return `${day}, ${month} ${todayDate} `;
 }
 
+function formatForecastDay(timestamp){
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+return days[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class="row">`;
   
-  days.forEach(function(forecastDay) {
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function(forecastDay, index) {
+    if (index < 3) {
   forecastHTML = forecastHTML + 
-    ` 
-                  <div class="col-4">
-                    ${forecastDay.dt}
-                    <img class="icon-1" src="img/icons/${forecastDay.data.weather[0].icon}.png" alt="windy" />
-                  </div>
-                  
-                 `;
+    `             <div class="col-4">
+                    ${formatForecastDay(forecastDay.dt)}
+                    <img  src="img/icons/${forecastDay.weather[0].icon}.png" alt="windy" width="40px"/>
+                  </div>`;
+  }
+  });
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
-  });
+  
+}
+
+function getForecast(coordinates) {
+  let apiKey = "656ac87c5034b9f4933b4a4211cbca36";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 
@@ -57,7 +71,10 @@ function showTemperature(response){
   document.querySelector(
     ".weatherApp"
   ).style.backgroundImage = `url("img/iconsMain/${response.data.weather[0].icon}.png")`;
+
+  getForecast(response.data.coord);
 }
+
 
 
 function search(city) {
@@ -137,7 +154,7 @@ let city3 = document.querySelector("#city3");
 city3.addEventListener("click", searchCity3);
 
 search("Lisbon");
-displayForecast();
+
 
 
 
